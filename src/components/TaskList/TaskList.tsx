f1101246afd2930f96d2f9908520b1ce1b0b3c9c
Sekouba CAMARA <sekoubacamara100@gmail.com>
2026-02-24 19:34:01 -0500

    Implement TaskList, and TaskItem

---
+++ src/components/TaskList/TaskList.tsx
@@ -0,0 +1,25 @@
+import type { TaskListProps } from "../../types";
+import { TaskItem } from "./TaskItem";
+
+function TaskList({ tasks, onStatusChange, onDelete }: TaskListProps) {
+  if (tasks.length === 0) {
+    return <p>No tasks available.</p>;
+  }
+  const taskElement = tasks.map((task) => (
+    <li key={task.id} style={{ marginBottom: "10px" }}>
+      <TaskItem
+        task={task}
+        onStatusChange={onStatusChange}
+        onDelete={onDelete}
+      />
+    </li>
+  ));
+
+  return (
+    <>
+      <ul style={{ listStyle: "none", padding: 0 }}>{taskElement}</ul>
+    </>
+  );
+}
+
+export { TaskList };
